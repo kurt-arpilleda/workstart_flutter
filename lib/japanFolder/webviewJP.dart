79f2942a454d14c198fcabba186e1703385c17dd
Kurt Bryant <BCArpilleda@gmail.com>
2025-05-24 08:45:37 +0800

    refresh

--- lib/japanFolder/webviewJP.dart
+++ lib/japanFolder/webviewJP.dart
@@ -396,6 +396,7 @@ class _SoftwareWebViewScreenState extends State<SoftwareWebViewScreenJP> with Wi
             pdfUrl: url,
             fileName: fileName,
             languageFlag: _currentLanguageFlag ?? 1,
+            shouldDeleteOnClose: true,
           ),
         ),
       );
